4394afc1fc9c961e308346ece843991ea8cdb762
sufian <sufian4se@gmail.com>
2021-04-02 21:17:27 +0600

    And Or shorthand

---
+++ chapter_three/027.logicalAndOr.js
@@ -0,0 +1,22 @@
+var name = ''
+
+
+var fullname = name || 'Abu sufian'
+
+// if (name.length === 0) {
+//     fullname = 'Abu sufian'
+// } else {
+//     fullname = name
+// }
+console.log(fullname);
+
+var isOk = true
+var a = 20
+var b = 10
+
+// if (isOk) {
+//     console.log('Everything is ok');
+// }
+
+isOk && console.log('Everything is OK');
+(a > b) && console.log('Statement is True');
