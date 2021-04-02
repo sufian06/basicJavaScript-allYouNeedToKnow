ccb238f7e11278691401eeacff199bfdf29aa143
sufian <sufian4se@gmail.com>
2021-04-02 20:53:44 +0600

    #026

---
+++ chapter_three/ternary.js
@@ -0,0 +1,17 @@
+var n = 10;
+var str = '';
+
+// if (n % 2 === 0) {
+//     str = 'EVEN'
+// } else {
+//     str = 'ODD'
+// }
+
+// console.log(str);
+
+// condition ? true side : false side
+var result = n % 2 === 0 ? 'Even' : 'Odd'
+
+
+console.log(result);
+
