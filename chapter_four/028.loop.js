f8c53641fc469fa4b8801d8d4ef7c31e2dede612
sufian <sufian4se@gmail.com>
2021-04-03 20:13:29 +0600

    loop.js change some text format

--- chapter_four/028.loop.js
+++ chapter_four/028.loop.js
@@ -1,4 +1,7 @@
-There is Mainly Three Types of Loops
-    1. For Loop
-    2. While Loop
-    3. Do while Loop
+
+/**
+ * There is Mainly Three Types of Loops
+ *  1. For Loop
+ * 2. While Loop
+ * 3. Do while Loop
+ */
