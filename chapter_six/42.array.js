230dcfc0aa976ebf84b1eec880eaafa1339cc4e1
sufian <sufian4se@gmail.com>
2021-04-20 22:26:10 +0600

    Array Decliaration Class #41 & 42

---
+++ chapter_six/42.array.js
@@ -0,0 +1,24 @@
+/**
+ * Advance Level Start
+ * Chapter Six: Working with Arrays
+ * 
+ * Array is a Data Stracture where we can organaize multipule data a single variable
+ */
+
+// 
+var arr = [1, 2, 3, 4, 5]
+arr[5] = 6
+arr[9] = 10
+arr[10] = 11
+arr[2] = 22
+
+console.log(arr[0]);
+console.log(arr[4]);
+console.log(arr[7]);
+console.log(arr);
+console.log(arr.length);
+
+
+var arr2 = Array(1, 2, 3)
+
+console.log(arr2);
