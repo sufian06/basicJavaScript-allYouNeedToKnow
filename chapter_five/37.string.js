5c8b7d0b4713ac763477496481553a49a8fb9696
sufian <sufian4se@gmail.com>
2021-04-20 20:05:30 +0600

    Escape Notation Class#38

--- chapter_five/37.string.js
+++ chapter_five/37.string.js
@@ -11,3 +11,20 @@ var n = 10;
 var str3 = String(n);
 
 console.log(str3);
+
+//  ****** Escape Notation Class #38
+
+var str4 = 'This is a \'String\'';      //Single or Double Quote
+var str5 = 'This is a \nString';        // New Line
+var str6 = 'This is a \tString';        // Tabe
+var str7 = 'This is a \\String';        // Back slash\
+
+// Carrige Return  \r
+// Vartical tab    \v
+// Backspace       \b
+// From Feed       \f
+
+console.log(str4);
+console.log(str5);
+console.log(str6);
+console.log(str7);
