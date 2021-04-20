438aac6994dce378c9be6acc3b9bd1ded8f12a52
sufian <sufian4se@gmail.com>
2021-04-20 23:00:02 +0600

    traverse file added

---
+++ chapter_six/44.traverse.js
@@ -0,0 +1,30 @@
+/**
+ * Traverse an Array
+ */
+
+var arr = [4, 5, 1, 6, 8, 9, 40]
+
+// arr[0]
+// arr[1]
+// arr[2]
+// arr[3]
+// arr[arr.length - 1]
+
+// for (var i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]);
+//     arr[i] = arr[i] +2
+// }
+
+// console.log(arr);
+
+var sum = 0
+for (var i = 0; i < arr.length; i++) {
+    sum += arr[i]
+}
+console.log(sum);
+
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        console.log(arr[i]);
+    }
+}
