a11de251beaa5b5a11b618b7bd6fa62d28d97802
sufian <sufian4se@gmail.com>
2021-04-20 19:49:16 +0600

    string chapter

---
+++ chapter_five/37.string.js
@@ -0,0 +1,13 @@
+
+
+// What is String?
+
+var str = 'Something';   //String Literal
+var str2 = String('Something')  //String Constructor
+
+console.log(str2);
+
+var n = 10;
+var str3 = String(n);
+
+console.log(str3);
