2be224034f5caa9bbe4178ec2bf81f0afc42c420
sufian <sufian4se@gmail.com>
2021-04-20 23:33:24 +0600

    added some statement

--- chapter_six/44.traverse.js
+++ chapter_six/44.traverse.js
@@ -2,7 +2,7 @@
  * Traverse an Array
  */
 
-var arr = [4, 5, 1, 6, 8, 9, 40]
+var arr = [4, 5, 1, 6, 8, 9, 40, 10, 11]
 
 // arr[0]
 // arr[1]
@@ -28,3 +28,28 @@ for (var i = 0; i < arr.length; i++) {
         console.log(arr[i]);
     }
 }
+
+// Odd  number
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 1) {
+        console.log(arr[i]);
+    }
+}
+
+// sum of odd number
+var sum2 = 0;
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 1) {
+        sum2 += arr[i]
+    }
+}
+console.log(sum2);
+
+// sum of even number
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        sum += arr[i]
+    }
+}
+console.log(sum);
