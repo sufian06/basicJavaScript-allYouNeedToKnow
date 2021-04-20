df22ac9df2bcb9fb8c1f87052b8b9c0caff63c28
sufian <sufian4se@gmail.com>
2021-04-20 20:54:19 +0600

    Class# 39 & 40 String Comparison

---
+++ chapter_five/39.string-comparison.js
@@ -0,0 +1,20 @@
+// ***** String Comparison *****//
+
+var a = 'I am'
+var b = 'Abu Sufian'
+
+console.log('001' == 1);
+
+var c = a.concat(' ' , b)
+var d = c.substr(5)
+
+console.log(c);
+// console.log(d);
+
+console.log(c.startsWith('I'));
+console.log(c.endsWith('Sufian'));
+console.log('sufian'.toUpperCase());
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log('          as soon as possible         '.trim());
+console.log(c.split(' '));      //Make sentence to word
