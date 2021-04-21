b5ac21226b924e0ddbe596dfeb471413f469b677
sufian <sufian4se@gmail.com>
2021-04-21 19:43:19 +0600

    Object & Declaration Class# 50 & 51

---
+++ chapter_seven/50.objects.js
@@ -0,0 +1,43 @@
+/**
+ * Chapter Seven: Objects of Javascript
+ */
+
+/**
+ * student: 1
+ * math: 80
+ * chemistry: 75
+ * physics: 86
+ * higher math: 90
+ * 
+ * [80, 78, 86, 90]             //Array
+ */
+
+// var obj = {}
+// obj.x = 10
+
+// console.log(obj);
+
+var point = {
+    x: 10,
+    y: 20
+}
+
+// point.y = 30
+point.z = 30
+
+
+console.log(point);
+
+var obj = Object()
+obj.a = 10;
+obj.b = 20;
+obj.c = 10;
+obj.d = 20;
+console.log(obj);
+
+var obj2 = new Object();
+obj2.i = 11;
+obj2.j = 12;
+obj2.k = 13;
+obj2.l = 14;
+console.log(obj2);
