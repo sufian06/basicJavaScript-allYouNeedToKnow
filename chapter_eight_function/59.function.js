38cefd95cd0fb2cc4d569bcb5f2ae6e3bb491d9b
sufian <sufian4se@gmail.com>
2021-04-22 17:03:37 +0600

    Function

---
+++ chapter_eight_function/59.function.js
@@ -0,0 +1,29 @@
+/**
+ * Chapter Eight: Functions in Javascript
+ * 
+ * What is function?
+ *  - Function is a masine whice has 3 parts, those are Input, Output, Processing
+ */
+
+
+// var date = new Date()
+// date.getFullYear()
+// console.log(date);
+
+function functionName() {
+
+}
+
+function add() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
+add()
+
+function sub() {
+    var a = 50;
+    var b = 20;
+    console.log(a - b);
+}
+sub()
