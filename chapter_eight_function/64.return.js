2db26fc71b70ea22285c751d19bacb63af592bc2
sufian <sufian4se@gmail.com>
2021-04-24 19:46:08 +0600

    return js

---
+++ chapter_eight_function/64.return.js
@@ -0,0 +1,25 @@
+/**
+ * Return Something from a Function
+ */
+
+ function addAll () {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+    return sum;
+}
+
+var result = addAll(1, 2, 3)
+console.log(result);
+
+
+function person (name, email) {
+    return {
+        name: name,
+        email: email
+    }
+}
+
+var p1 = person ('Abu Sufian', 'sufian4se@gmail.com')
+console.log(p1);
