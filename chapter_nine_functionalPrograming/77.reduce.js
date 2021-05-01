949216df313c6578af5ec995fbba2b7281501f89
sufian <sufian4se@gmail.com>
2021-05-01 18:31:18 +0600

    Reduce JS

---
+++ chapter_nine_functionalPrograming/77.reduce.js
@@ -0,0 +1,39 @@
+/**
+ * Reduce JS
+ */
+var arr = [100, 1, 2, 45, 3, 4, 5]
+
+// var sum = arr.reduce(function(prev, curr) {
+//     return prev + curr
+// }, 100)
+
+// var max = arr.reduce(function (prev, curr) {
+//     return Math.max(prev, curr)
+// }, 0)
+
+// console.log(sum);
+// console.log(max);
+
+/** Emplement */
+
+function myReduce(arr, cb, acc) {
+
+    for (var i = 0; i < arr.length; i++) {
+        acc = cb (acc, arr[i])
+    }
+    return acc
+}
+
+var sum = myReduce(arr, function(prev, curr) {
+    return prev + curr
+}, 0)
+
+var max = myReduce(arr, function(prev, curr) {
+    return Math.max(prev, curr)
+}, 0)
+
+var min = myReduce(arr, function(prev, curr) {
+    return Math.min(prev, curr)
+}, arr[0])
+
+console.log(sum, max, min);
